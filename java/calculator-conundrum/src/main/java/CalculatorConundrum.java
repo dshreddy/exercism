f5beee5bd22117914e1f8d6e60595c27b4255f4e
D Sai Hemanth Reddy <dshr@Ds-MacBook-Air.local>
2026-02-17 01:31:30 +0530

    Finished Exceptions

---
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -0,0 +1,29 @@
+import static java.lang.String.format;
+
+class CalculatorConundrum {
+    public String calculate(int operand1, int operand2, String operation) {
+
+        if(operation == null) throw new IllegalArgumentException("Operation cannot be null");
+        if(operation.isEmpty()) throw new IllegalArgumentException("Operation cannot be empty");
+
+        int result;
+        switch (operation) {
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "/":
+                try{
+                    result = operand1 / operand2;
+                } catch (ArithmeticException e) {
+                    throw new IllegalOperationException("Division by zero is not allowed", e);
+                }
+                break;
+            default:
+                throw new IllegalOperationException(format("Operation '%s' does not exist", operation));
+        }
+        return format("%d %s %d = %d", operand1, operation, operand2, result);
+    }
+}
