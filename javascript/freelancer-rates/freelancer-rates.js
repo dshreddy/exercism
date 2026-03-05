e162a5a2ce83f7e202e36131bc0f811b7dd4e222
Sai Hemanth Reddy <dshreddy03@gmail.com>
2026-03-05 12:17:53 +0530

    Updated README

---
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -0,0 +1,41 @@
+const WORKING_HOURS_PER_DAY = 8;
+const BILLABLE_DAYS_PER_MONTH = 22;
+
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+  return WORKING_HOURS_PER_DAY * ratePerHour;
+}
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+export function daysInBudget(budget, ratePerHour) {
+  return Math.floor(budget/dayRate(ratePerHour));
+}
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ *
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+
+  var normalRatePerDay = dayRate(ratePerHour);
+  var discountedRatePerDay = normalRatePerDay * (1-discount);
+  var noDiscountDays = numDays%22;
+  var discountDays = numDays - noDiscountDays;
+
+  return Math.ceil((discountDays * discountedRatePerDay) + (noDiscountDays * normalRatePerDay)); 
+}
